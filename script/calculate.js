48f792a04ccd39e0bae28a732688333da7bc154f
RickyPWebDev <100093181+RickyPWebDev@users.noreply.github.com>
2024-03-12 21:50:51 +0000

    Add files via upload

---
+++ script/calculate.js
@@ -0,0 +1,24 @@
+let outPut = document.getElementById('calculation-output');
+
+function calculator() {
+    let income1 = parseInt(document.getElementById('income-1').value);
+    let income2 = parseInt(document.getElementById('income-2').value);
+    const entitlement = 1668
+    const allowance = 379
+    let totalIncome =  income1 + income2;
+    let total = totalIncome - allowance;
+    let deduction = 0.55 * total;
+
+    let payment = Math.round(entitlement - deduction) 
+
+
+    if(payment > 1565) {
+        payment = 1565
+    }
+    if(payment < 0) {
+        payment = 0
+    }
+
+
+    outPut.innerHTML = `With a total household income of £${totalIncome} you are entitled to £${payment} `
+}
