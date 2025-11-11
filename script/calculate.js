06dff0db664c4bda66c061eb053d3980153d56b2
RickyPWebDev <100093181+RickyPWebDev@users.noreply.github.com>
2025-11-11 21:14:10 +0000

    Update calculate.js

--- script/calculate.js
+++ script/calculate.js
@@ -6,8 +6,8 @@ let entitlementChart = null;
 function calculator() {
     let income1 = parseInt(document.getElementById('income-1').value);
     let income2 = parseInt(document.getElementById('income-2').value);
-    const entitlement = 1764 // + 318
-    const allowance = 411
+    const entitlement = 1764 + 318;
+    const allowance = 411;
     let totalIncome =  income1 + income2;
     let total = totalIncome - allowance;
     let deduction = 0.55 * total;
